e4edb66a21b163083b3528e1efdbbc8223a29a1d
Alberto Redondo <albertoredondoalvarez@gmail.com>
2025-03-24 00:51:56 +0000

    List of Users and Backend of Events added

---
+++ backend/src/main/java/ulpgc/pigs/backend/controller/EventController.java
@@ -0,0 +1,44 @@
+package ulpgc.pigs.backend.controller;
+
+import org.springframework.web.bind.annotation.*;
+import ulpgc.pigs.backend.entity.Event;
+import ulpgc.pigs.backend.service.EventService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/events")
+public class EventController {
+
+    private final EventService eventService;
+
+    public EventController(EventService eventService) {
+        this.eventService = eventService;
+    }
+
+    @PostMapping
+    public Event createEvent(@RequestBody Event event) {
+        return eventService.createEvent(event);
+    }
+
+    @GetMapping
+    public List<Event> getAllEvents() {
+        return eventService.getAllEvents();
+    }
+
+    @GetMapping("/{id}")
+    public Event getEventById(@PathVariable Long id) {
+        return eventService.getEventById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Event updateEvent(@PathVariable Long id, @RequestBody Event eventDetails) {
+        return eventService.updateEvent(id, eventDetails);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteEvent(@PathVariable Long id) {
+        eventService.deleteEvent(id);
+    }
+}
+
